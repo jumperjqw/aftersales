ca288fb0f5766cb62e8c93844232e192d86a9e2d
jumperjqw <394319168@qq.com>
2020-04-27 21:06:43 +0800

    用户查改和问题增查

--- After_Sales/src/main/java/com/zgl/aftersales/pojo/Users.java
+++ After_Sales/src/main/java/com/zgl/aftersales/pojo/Users.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Users {
+public class Users implements Serializable {
     private int User_id;
     private String User_name;
     private String Password;
@@ -16,5 +18,6 @@ public class Users {
     private String Email;
     private int Task_num;
     private int Role_id;
+
 
 }
